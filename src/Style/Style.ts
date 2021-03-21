a7c13a4f915603fc109a59e8b7f0dce86a357f09
Nino Chimento <saninoc84@gmail.com>
2021-03-21 14:30:19 +0100

    added menu top

--- src/Style/Style.ts
+++ src/Style/Style.ts
@@ -108,3 +108,37 @@ export const StyledInput = Styled.input`
         outline: none;
     }
 `;
+export const StyledNav = Styled.div`
+    width:90%;
+    margin:auto;
+    justify-content:space-between;
+    align-items:center;
+    height:90px;
+`;
+export const StyledLogo = Styled.div`
+    width:150px;
+`;
+
+export const StyledTextJumbo = Styled.div`
+    position:absolute;
+    left:10%;
+    bottom:10%;
+    line-height:42px;
+`;
+
+export const StyledTitleJumbo = Styled.div`
+     font-size:45px;
+     color:white;
+     text-transform:uppercase;
+`;
+export const StyleTextJumbo = Styled.div`
+    font-size:28px;
+    color:white;
+    text-transform: uppercase;
+    width:90%;
+    line-height: 25px;
+`;
+export const StyledSpanJumbo = Styled.span`
+    color: #E7C700;
+    text-transform: uppercase;
+`;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -8,10 +8,19 @@ import { FormStep2 } from "../components/FormStep2";
 import { FormStep3 } from "../components/FormStep3";
 import { FormStep4 } from "../components/FormStep4";
 import "@fortawesome/fontawesome-free/js/all.js";
-
+import {
+  StyledLogo,
+  StyledNav,
+  StyledSpanJumbo,
+  StyledTextJumbo,
+  StyledTitleJumbo,
+  StyleTextJumbo,
+} from "../Style/Style";
+import img from "../images/testataLanding.png";
 const StyledWrap = Styled.div`
   position:relative;
   height:700px;
+  background-image: url(${img});
 `;
 
 const StyledWrapForm = Styled.div`
@@ -150,65 +159,89 @@ export const Landing: FC = (props) => {
     },
   ];
   return (
-    <StyledWrap>
-      <Img src={require("../images/testataLanding.png")} />
-      <StyledWrapForm>
-        <BreadForm
-          handleSelect={setBredSelect}
-          active={bredSelect}
-          options={OptionsBread}
-        />
-        <StyledForm>
-          {bredSelect === 1 && (
-            <FormStep1
-              handletotalActive={setTotalSelect}
-              totalActive={totalSelect}
-              total={total}
-              handleMyTotal={(e: any) => setMyTotal(e.target.value)}
-            />
-          )}
-          {bredSelect === 2 && (
-            <FormStep2
-              optionActive={optionWork}
-              handleOptionActive={setOptionWork}
-              options={optionsWork}
-            />
-          )}
-          {bredSelect === 3 && (
-            <FormStep3
-              handleChangeData={(e: any) => setData(e.target.value)}
-              handleChangeMonths={(e: any) => setMonths(e.target.value)}
-              handleChangeTypeWork={(e: any) => setTypeWork(e.target.value)}
-              handleChangeSalary={(e: any) => setSalary(e.target.value)}
-            />
-          )}
-          {bredSelect === 4 && <FormStep4 />}
-          {bredSelect === 1 && (
-            <StyledButton onClick={handleUp}>
-              AVANTI
-              <StyleRow className="fas fa-arrow-right"></StyleRow>
-            </StyledButton>
-          )}
-          {bredSelect !== 1 && (
-            <StyledWrapButtons>
-              <StyledButtonNoFirst variant="down" onClick={handleDown}>
-                INDIETRO
-              </StyledButtonNoFirst>
-              {bredSelect !== 4 && (
-                <StyledButtonNoFirst onClick={handleUp}>
-                  AVANTI
-                  <StyleRow className="fas fa-arrow-right"></StyleRow>
+    <>
+      <StyledNav className="d-flex jus">
+        <StyledLogo>
+          <Img src={require("../images/logo.svg")} />
+        </StyledLogo>
+        <nav className="navbar navbar-expand-sm">
+          <ul className="navbar-nav">
+            <li className="nav-item mr-3">Prestiti Fai da te</li>
+            <li className="nav-item mr-3">Tasso Fisso agevolato</li>
+            <li className="nav-item mr-3">Vantaggi</li>
+            <li className="nav-item mr-3">Vantaggi</li>
+            <li className="nav-item mr-3">Fai Da te</li>
+            <li className="nav-item mr-3">Faq</li>
+          </ul>
+        </nav>
+      </StyledNav>
+      <StyledWrap>
+        <StyledTextJumbo>
+          <StyledTitleJumbo>100% ONLINE</StyledTitleJumbo>
+          <StyledTitleJumbo>FACILE VELOCE DA CASA</StyledTitleJumbo>
+          <StyleTextJumbo>
+            <StyledSpanJumbo>Richiedere un prestito</StyledSpanJumbo> non e mai
+            stato cosi facile
+          </StyleTextJumbo>
+        </StyledTextJumbo>
+        <StyledWrapForm>
+          <BreadForm
+            handleSelect={setBredSelect}
+            active={bredSelect}
+            options={OptionsBread}
+          />
+          <StyledForm>
+            {bredSelect === 1 && (
+              <FormStep1
+                handletotalActive={setTotalSelect}
+                totalActive={totalSelect}
+                total={total}
+                handleMyTotal={(e: any) => setMyTotal(e.target.value)}
+              />
+            )}
+            {bredSelect === 2 && (
+              <FormStep2
+                optionActive={optionWork}
+                handleOptionActive={setOptionWork}
+                options={optionsWork}
+              />
+            )}
+            {bredSelect === 3 && (
+              <FormStep3
+                handleChangeData={(e: any) => setData(e.target.value)}
+                handleChangeMonths={(e: any) => setMonths(e.target.value)}
+                handleChangeTypeWork={(e: any) => setTypeWork(e.target.value)}
+                handleChangeSalary={(e: any) => setSalary(e.target.value)}
+              />
+            )}
+            {bredSelect === 4 && <FormStep4 />}
+            {bredSelect === 1 && (
+              <StyledButton onClick={handleUp}>
+                AVANTI
+                <StyleRow className="fas fa-arrow-right"></StyleRow>
+              </StyledButton>
+            )}
+            {bredSelect !== 1 && (
+              <StyledWrapButtons>
+                <StyledButtonNoFirst variant="down" onClick={handleDown}>
+                  INDIETRO
                 </StyledButtonNoFirst>
-              )}
-              {bredSelect === 4 && (
-                <StyledButtonConfirm onClick={sendRequest}>
-                  CONFERMA
-                </StyledButtonConfirm>
-              )}
-            </StyledWrapButtons>
-          )}
-        </StyledForm>
-      </StyledWrapForm>
-    </StyledWrap>
+                {bredSelect !== 4 && (
+                  <StyledButtonNoFirst onClick={handleUp}>
+                    AVANTI
+                    <StyleRow className="fas fa-arrow-right"></StyleRow>
+                  </StyledButtonNoFirst>
+                )}
+                {bredSelect === 4 && (
+                  <StyledButtonConfirm onClick={sendRequest}>
+                    CONFERMA
+                  </StyledButtonConfirm>
+                )}
+              </StyledWrapButtons>
+            )}
+          </StyledForm>
+        </StyledWrapForm>
+      </StyledWrap>
+    </>
   );
 };
